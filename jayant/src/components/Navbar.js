37261a324753baa46496c1c7e8a766a9b56d93e2
jayantwaldia <imjayantwaldia@gmail.com>
2022-07-13 11:57:30 +0530

    Added socials

--- jayant/src/components/Navbar.js
+++ jayant/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Navbar.css";
-import image from "../assets/linkedin.svg";
+import linkedin from "../assets/linkedin.svg";
+import github from "../assets/github.svg";
+import twitter from "../assets/twitter.svg";
 
 function Navbar() {
   return (
@@ -18,12 +20,20 @@ function Navbar() {
       </div>
       <div className="Links">
         <div className="Link">
-          <a href="https://www.linkedin.com/in/jayantwaldia/">
-            <img src={image}></img>
+          <a href="https://www.linkedin.com/in/jayantwaldia/" target="_blank">
+            <img src={linkedin} />
           </a>
         </div>
-        <div className="Link"></div>
-        <div className="Link"></div>
+        <div className="Link">
+          <a href="https://github.com/jayantwaldia" target="_blank">
+            <img src={github} />
+          </a>
+        </div>
+        <div className="Link">
+          <a href="https://twitter.com/jayantwaldia" target="_blank">
+            <img src={twitter} />
+          </a>
+        </div>
       </div>
     </div>
   );
