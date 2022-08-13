b42ff6ebd616b7e6c4292254d3cad4b6e525153f
jayantwaldia <imjayantwaldia@gmail.com>
2022-08-13 08:35:23 +0530

    Visual changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import linkedin from "../assets/linkedin.svg";
 import github from "../assets/github.svg";
 import twitter from "../assets/twitter.svg";
-import BlogPage from "../pages/BlogPage";
 
 function Page({ name, source, id }) {
   return (
