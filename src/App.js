d2fda31e5ed577f1ddc68142d136ba375821b7a2
jayantwaldia <imjayantwaldia@gmail.com>
2022-09-02 20:27:40 +0530

    added blogs inside the portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BlogPage from "./pages/BlogPage";
 import AboutPage from "./pages/AboutPage";
+import BlogPage1 from "./pages/BlogPage1";
 
 function App() {
   return (
@@ -23,6 +23,7 @@ function App() {
             }
           ></Route>
           <Route exact path="/blog" element={<BlogPage />}></Route>
+          <Route exact path="/blog/1" element={<BlogPage1 />}></Route>
           <Route exact path="/about" element={<AboutPage />}></Route>
         </Routes>
       </div>
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,19 +1,64 @@
+import { NavLink } from "react-router-dom";
+import gem from "../assets/gem.ico";
 import Card from "../shared/Card";
 
 function BlogPage() {
   return (
-    <>
-      <div className="Main">
-        <p className="Page-Heading">Blog </p>
-        <p className="Description">
-          Not heavy with words, but here are some thoughts I randomly cross by
-          on the Internet
-        </p>
-      </div>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-    </>
+    <div className="Main">
+      <p className="Page-Heading">Blog </p>
+      <p className="Description">
+        Not heavy with words, but here are some thoughts I randomly cross by on
+        the Internet
+      </p>
+
+      <NavLink to={"/blog/1"} className="nav-link">
+        <Card>
+          <h4>
+            <img
+              style={{ width: "15px", marginRight: "10px" }}
+              src={gem}
+              alt="blue-gem"
+            ></img>
+            Being Yourself as a career
+          </h4>
+        </Card>
+      </NavLink>
+      <NavLink to={"/blog/1"} className="nav-link">
+        <Card>
+          <p>
+            <img
+              style={{ width: "15px", marginRight: "10px" }}
+              src={gem}
+              alt="blue-gem"
+            ></img>
+          </p>
+        </Card>
+      </NavLink>
+      <NavLink to={"/blog/1"} className="nav-link">
+        <Card>
+          <p>
+            <img
+              style={{ width: "15px", marginRight: "10px" }}
+              src={gem}
+              alt="blue-gem"
+            ></img>
+            A career.
+          </p>
+        </Card>
+      </NavLink>
+      <NavLink to={"/blog/1"} className="nav-link">
+        <Card>
+          <p>
+            <img
+              style={{ width: "15px", marginRight: "10px" }}
+              src={gem}
+              alt="blue-gem"
+            ></img>
+            Being Yourself as a career.
+          </p>
+        </Card>
+      </NavLink>
+    </div>
   );
 }
 
