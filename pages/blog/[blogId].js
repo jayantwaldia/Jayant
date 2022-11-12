29f279c98726f12d8158db0a6a5f1248de2ae0f2
Jayant Waldia <imjayantwaldia@gmail.com>
2022-11-12 15:20:44 +0530

    clean up

--- pages/blog/[blogId].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Blog() {
-  const router = useRouter();
-  const blogId = router.query.blogId;
-  return <h1>yolo {blogId}</h1>;
-}
--- pages/docs/[[...params]].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Doc() {
-  const router = useRouter();
-  const { params = [] } = router.query;
-
-  return <h1>Hey</h1>;
-}
